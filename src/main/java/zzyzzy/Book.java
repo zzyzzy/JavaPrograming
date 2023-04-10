37e36bf5c7811ae8a9cedf4407f4e7b0a892d9c9
zzyzigy <zzyzigy@gmail.com>
2023-04-10 15:42:33 +0900

    도서 쇼핑몰 예제 v4 Book 클래스 작성 도서정보 등록 및 조회

---
+++ src/main/java/zzyzzy/Book.java
@@ -0,0 +1,84 @@
+package zzyzzy;
+
+public class Book {
+    private String isbn;
+    private String bkname;
+    private int price;
+    private String author;
+    private String desc;
+    private String bkdept;
+    private String pubdate;
+
+    public Book(String isbn, String bkname, int price, String author, String desc, String bkdept, String pubdate) {
+        this.isbn = isbn;
+        this.bkname = bkname;
+        this.price = price;
+        this.author = author;
+        this.desc = desc;
+        this.bkdept = bkdept;
+        this.pubdate = pubdate;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getBkname() {
+        return bkname;
+    }
+
+    public void setBkname(String bkname) {
+        this.bkname = bkname;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getBkdept() {
+        return bkdept;
+    }
+
+    public void setBkdept(String bkdept) {
+        this.bkdept = bkdept;
+    }
+
+    public String getPubdate() {
+        return pubdate;
+    }
+
+    public void setPubdate(String pubdate) {
+        this.pubdate = pubdate;
+    }
+
+    @Override
+    public String toString() {
+        String fmt = "%s %s %d %s\n%s %s %s";
+        return String.format(fmt, isbn, bkname, price,
+                author, desc, bkdept, pubdate);
+    }
+}
--- src/main/java/zzyzzy/BookMarketService.java
+++ src/main/java/zzyzzy/BookMarketService.java
@@ -5,9 +5,11 @@ import java.util.Scanner;
 public class BookMarketService {
     private Scanner sc = null;
     private MemberShip m = null;  // 회원정보 저장용
+    private Book[] books = null;  // 도서정보 저장용
 
     public BookMarketService() {
         sc = new Scanner(System.in);
+        registerBooks();    // 도서 생성
     }
 
     // bookmarket 메뉴 작성
@@ -38,12 +40,15 @@ public class BookMarketService {
                 System.out.println(m);
                 break;
             case "2":
-                System.out.println("\n장바구니 상품 목록\n");
-                break;
+                System.out.println("\n장바구니 상품 목록\n"); break;
             case "3":
                 System.out.println("\n장바구니 비우기\n");break;
             case "4":
-                System.out.println("\n장바구니 상품 추가\n");break;
+                System.out.println("\n장바구니 상품 추가\n");
+                for (Book b : books) {
+                    System.out.println(b);
+                }
+                break;
             case "5":
                 System.out.println("\n주문수량 변경\n");break;
             case "6":
@@ -68,6 +73,24 @@ public class BookMarketService {
         String hp = sc.next();
         
         m = new MemberShip(230410, name, email, hp);
+    }
+
+    // 도서정보 자동등록
+    public void registerBooks() {
+        books = new Book[3];
+
+        books[0] = new Book(
+            "2304101234", "JSP웹프로그래밍",
+            27000, "송미영", "단계별로 쇼핑몰 구현",
+            "IT전문서", "2018-10-08");
+        books[1] = new Book(
+            "2304104567", "안드로이드 프로그래밍",
+            33000, "우재남", "실습 멘토링 제공",
+            "IT전문서", "2022-01-22");
+        books[2] = new Book(
+            "2304107891", "스크래치",
+            22000, "고광일", "사고력을 키우는 코딩",
+            "컴퓨터입문", "2019-06-19");
     }
 
 }
