ce0cbbc3b495449e20e0bc310b29583640d723d8
zzyzigy <zzyzigy@gmail.com>
2023-04-10 18:09:56 +0900

    성적처리 프로그램 v1 성적 추가, 조회 기능 구현

--- src/main/java/zzyzzy/SungJukV1Service.java
+++ src/main/java/zzyzzy/SungJukV1Service.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class SungJukV1Service {
     private Scanner sc = null;
     private SungJukVO[] sjs = null;
+    private int idx = 0;
 
     public SungJukV1Service() {
         sc = new Scanner(System.in);
@@ -33,14 +34,64 @@ public class SungJukV1Service {
 
     public void processMenu(String menu) {
         switch (menu) {
-            case "1": break;
-            case "2": break;
-            case "3": break;
-            case "4": break;
-            case "5": break;
+            case "1": newSungJuk(); break;
+            case "2": readSungJuk(); break;
+            case "3": readOneSungJuk(); break;
+            case "4": modifySungJuk(); break;
+            case "5": removeSungJuk(); break;
             case "0": System.exit(0); break;
             default:
                 System.out.println("\n>> 잘못 입력하셨습니다! <<\n");
+        }
+    }
+
+    private void removeSungJuk() {
+    }
+
+    private void modifySungJuk() {
+        
+    }
+
+    private void readOneSungJuk() {
+        
+    }
+
+    // 성적 리스트 조회 (이름,국어,영어,수학)
+    private void readSungJuk() {
+        String fmt = " %s %d %d %d\n";
+        for (SungJukVO sj : sjs) {
+            if (sj != null)  // 배열에 성적데이터가 존재한다면
+                System.out.printf(fmt, sj.getName(),
+                    sj.getKor(), sj.getEng(), sj.getMat());
+        } // sjs 배열에 저장된 모든 성적데이터 출력
+    }
+
+    // 성적 데이터 추가
+    private void newSungJuk() {
+        System.out.print("이름은? ");
+        String name = sc.next();
+        System.out.print("국어는? ");
+        int kor = sc.nextInt();
+        System.out.print("영어은? ");
+        int eng = sc.nextInt();
+        System.out.print("수학은? ");
+        int mat = sc.nextInt();
+
+        SungJukVO sj = new SungJukVO(name, kor, eng, mat);
+        computeSungJuk(sj);    // 성적 처리
+        sjs[idx++] = sj;       // 처리된 성적데이터 배열에 저장
+    }
+
+    private void computeSungJuk(SungJukVO sj) {
+        sj.setTot( sj.getKor() + sj.getEng() + sj.getMat() );
+        sj.setAvg( (double) sj.getTot() / 3 );
+
+        switch ((int)(sj.getAvg()/10)) {
+            case 10: case 9: sj.setGrd('수'); break;
+            case 8: sj.setGrd('우'); break;
+            case 7: sj.setGrd('미'); break;
+            case 6: sj.setGrd('양'); break;
+            default: sj.setGrd('가'); break;
         }
     }
 
